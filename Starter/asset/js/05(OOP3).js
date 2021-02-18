74dd2e09267d2ad010c186f5fee919e5c3e68078
aben-bel <abenezer.sbelay@gmail.com>
2021-02-18 10:07:10 +0300

    oop3

--- Starter/asset/js/05(OOP3).js
+++ Starter/asset/js/05(OOP3).js
@@ -1,16 +1,16 @@
-// 1. Create a Person_ES6 Class 
+// 1. A Person Object for Base
+const PersonObj = {
+  greeting() {
+    return `Hi , ${this.firstName} ${this.lastName}`;
+  },
+};
 
+//2. Create Theon Object using Person Template
+const Theon = Object.create(PersonObj);
 
+//3. Add firstName[Theon] and lastName[Greyjoy]
+Theon.firstName = "Theon";
+Theon.lastName = "Greyjoy";
 
-//2. Create the constructor that initialize firstName and lastName
-
-
-//3. Define greeting() method that displays Full Name
-
-
-//4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
-
-
-
-//5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+//4. Display the greeting on Theon Object [remove the string when you have the object]
+Obj_C2.innerHTML = Theon.greeting();
