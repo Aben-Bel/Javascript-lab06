2bb8f88c29946052ed6fa328f1854b5227a378e6
aben-bel <abenezer.sbelay@gmail.com>
2021-02-18 09:42:30 +0300

    templating

--- Starter/asset/js/01(TemplateLiteral).js
+++ Starter/asset/js/01(TemplateLiteral).js
@@ -1,48 +1,36 @@
-//DOM Load 
-document.addEventListener('DOMContentLoaded', () => {
+//DOM Load
+document.addEventListener("DOMContentLoaded", () => {
+  templateStringConcatenation();
 
-    templateStringConcatenation();
-
-    displayTemplateExpression();
-
-
-})
-
+  displayTemplateExpression();
+});
 
 function templateStringConcatenation() {
+  // A Student Object
+  const student = { name: "Abebe", age: 21 };
 
-    // A Student Object 
-    const student = { name: "Abebe", age: 21 }
+  //String to Display
+  const fullIntro =
+    "My Name is " + student.name + " and i am " + student.age + " Years Old";
 
-    //String to Display 
-    const fullIntro = "My Name is " + student.name + " and i am " + student.age + " Years Old"
+  //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
+  var fullIntroTemplate = `My Name is ${student.name} and I am ${student.age} Years Old`;
 
-    //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
-
-
-    //3. Display it [change it to fullIntroTemplate]
-    templateConcatenationResult.innerHTML = fullIntro;
-
+  //3. Display it [change it to fullIntroTemplate]
+  templateConcatenationResult.innerHTML = fullIntroTemplate;
 }
 
 function displayTemplateExpression() {
+  //String to Display
+  const result = "The Sum of the Numbers is  : " + sum(1, 2);
 
+  //1. Create a variable called result1 and Store the same string displayed above with template literal
+  var result1 = `The Sum of the Numbers is : ${sum(1, 2)}`;
 
-    //String to Display 
-    const result = "The Sum of the Numbers is  : " + sum(1, 2)
-
-    //1. Create a variable called result1 and Store the same string displayed above with template literal
-
-
-    //3. Display it [change it to result1]
-    templateExpression.innerHTML = result;
-
-
-
+  //3. Display it [change it to result1]
+  templateExpression.innerHTML = result1;
 }
 
 function sum(x, y) {
-
-    return x + y;
-
+  return x + y;
 }
