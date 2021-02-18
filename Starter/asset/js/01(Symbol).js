1540450daef0ce62dccb5ab5a390776a8eadf2b2
aben-bel <abenezer.sbelay@gmail.com>
2021-02-18 09:42:06 +0300

    symbol

--- Starter/asset/js/01(Symbol).js
+++ Starter/asset/js/01(Symbol).js
@@ -1,60 +1,48 @@
-//DOM Load 
-document.addEventListener('DOMContentLoaded', () => {
+//DOM Load
+document.addEventListener("DOMContentLoaded", () => {
+  displaySymbol();
 
-    displaySymbol();
+  displaySymbolEquality();
 
-    displaySymbolEquality();
-
-    displaySymbolObject();
-
-
-
-})
-
+  displaySymbolObject();
+});
 
 function displaySymbol() {
+  // 1. Create an id constant that store Symbol with id debugging string
+  const id = Symbol("Debugging_String");
 
-    // 1. Create an id constant that store Symbol with id debugging string 
-
-    //2. Display the symbol on ui as string using toString()
-    symbolResult.innerHTML = `<p>${new Symbol("id debugging string")}</p>`;
-
+  //2. Display the symbol on ui as string using toString()
+  symbolResult.innerHTML = id.toString();
 }
 
 function displaySymbolEquality() {
+  // 1. Create an id1 constant that store Symbol with id debugging string
+  const id1 = Symbol("Debugging_String");
 
-    // 1. Create an id1 constant that store Symbol with id debugging string 
+  // 2. Create an id2 constant that store Symbol with id debugging string
+  const id2 = Symbol("Debugging_String");
 
+  // 3. Compare their equality (===) , store it on result variable
+  var result = id1 == id2;
 
-    // 2. Create an id2 constant that store Symbol with id debugging string 
-
-
-    // 3. Compare their equality (===) , store it on result variable
-
-
-    //2. Display the result variable [Remove the string once you have the variable]
-    symbolEquality.innerHTML = "result";
-
-
-
+  //2. Display the result variable [Remove the string once you have the variable]
+  symbolEquality.innerHTML = result;
 }
 
 function displaySymbolObject() {
+  // A Student Object
+  const student = { name: "Abebe", age: 21, year: "2dn year" };
 
-    // A Student Object 
-    const student = { name: "Abebe", age: 21, year: "2dn year" }
+  // 1. Create an id constant that store Symbol with id debugging string
+  const id = Symbol("Debugging_String");
 
-    // 1. Create an id constant that store Symbol with id debugging string 
+  //2. Add the symbol property to the object [this will be considered as hidden property]
+  //3. Let the Id value be 1234
+  // Note : user []
+  student[id] = 4567;
 
+  //Display the object on console
+  console.log(student);
 
-    //2. Add the symbol property to the object [this will be considered as hidden property]
-    //3. Let the Id value be 1234
-    // Note : user [] 
-
-
-    //Display the object on console 
-    console.log(student);
-
-    symbolObject.innerHTML = "View it On Console";
-
+  symbolObject.innerHTML = "View it On Console";
 }
